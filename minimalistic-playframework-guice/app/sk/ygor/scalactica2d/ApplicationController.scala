aee35c2874a4b3b84be8a6b0ea8b13b8d01ba8d7
ygor-sk <igor.inas@gmail.com>
2018-09-25 15:54:50 +0200

    guice

--- minimalistic-playframework-guice/app/sk/ygor/scalactica2d/ApplicationController.scala
+++ minimalistic-playframework-guice/app/sk/ygor/scalactica2d/ApplicationController.scala
@@ -4,27 +4,18 @@ import javax.inject.Inject
 import play.api.Configuration
 import play.api.mvc._
 
-import scala.concurrent.{ExecutionContext, Future}
-
 class ApplicationController @Inject()(configuration: Configuration,
-                                      executionContext: ExecutionContext,
-                                      playBodyParsers: PlayBodyParsers) {
+                                      controllerComponents: ControllerComponents)
+  extends AbstractController(controllerComponents) {
 
-  def foo(path: String): Action[String] = new play.api.mvc.Action[String] {
-    override def parser: BodyParser[String] = playBodyParsers.tolerantText
-
-    override def apply(request: Request[String]): Future[Result] = Future.successful(
-      Results.Ok(
-        Seq(
-          s"URI: ${request.method} ${request.uri}",
-          s"User agent: ${request.headers.get("User-Agent").getOrElse("Unknown")}",
-          s"Listening on port: ${configuration.get[String]("play.server.http.port")}",
-          s"Received body of size: ${request.body.length}",
-        ).mkString("\n")
-      )
+  def foo(path: String): Action[String] = Action(parse.tolerantText) { request =>
+    Ok(
+      Seq(
+        s"URI: ${request.method} ${request.uri}",
+        s"User agent: ${request.headers.get("User-Agent").getOrElse("Unknown")}",
+        s"Listening on port: ${configuration.get[String]("play.server.http.port")}",
+        s"Received body of size: ${request.body.length}",
+      ).mkString("\n")
     )
-
-    override def executionContext: ExecutionContext = ApplicationController.this.executionContext
-
   }
 }
